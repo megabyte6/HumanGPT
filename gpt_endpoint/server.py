3c5f379636d9632c9e969022ccce7c2ec087f880
Brayden Chan <braydenmchan@gmail.com>
2023-05-13 20:09:28 -0700

    Fix code style

--- gpt_endpoint/server.py
+++ gpt_endpoint/server.py
@@ -12,25 +12,24 @@ import gpt4free
 from gpt4free import Provider
 
 
-BIND_HOST = 'localhost'
+BIND_HOST = "localhost"
 PORT = 8008
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.write_response(b'')
+        self.write_response(b"")
 
     def do_POST(self):
-        print("recieved")
-        content_length = int(self.headers.get('content-length', 0))
+        print("received")
+        content_length = int(self.headers.get("content-length", 0))
         body = self.rfile.read(content_length)
-        print("Request:", body.decode('utf-8'))
-        GPTrequest = json.loads(body.decode('utf-8'))
-        response = gpt4free.Completion.create(Provider.You, prompt=GPTrequest['request'])
-        sendback = json.loads("{}")
-        sendback["response"] = response
+        print("Request:", body.decode("utf-8"))
+        gpt_request = json.loads(body.decode("utf-8"))
+        response = json.loads("{}")
+        response["response"] = gpt4free.Completion.create(Provider.You, prompt=gpt_request["request"])
 
-        self.write_response(sendback)
+        self.write_response(response)
 
     def write_response(self, content):
         self.send_response(200)
@@ -42,11 +41,11 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 if len(argv) > 1:
-    arg = argv[1].split(':')
+    arg = argv[1].split(":")
     BIND_HOST = arg[0]
     PORT = int(arg[1])
 
-print(f'Listening on http://{BIND_HOST}:{PORT}\n')
+print(f"Listening on http://{BIND_HOST}:{PORT}\n")
 
 httpd = HTTPServer((BIND_HOST, PORT), SimpleHTTPRequestHandler)
 httpd.serve_forever()
