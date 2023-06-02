ed7eb20c014aa6732585b87b7ee65d76d44871d8
peterpetep <86385378+peterpetep@users.noreply.github.com>
2023-06-02 14:24:05 -0700

    final final final bug fix plz work part 4

--- gpt_endpoint/server.py
+++ gpt_endpoint/server.py
@@ -27,7 +27,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         print("Request:", body.decode("utf-8"))
         gpt_request = json.loads(body.decode("utf-8"))
         response = json.loads("{}")
-        receivestuff(response)
+        self.receivestuff(response)
         
     async def receivestuff(response):
         response["response"] = await gpt4free.Completion.create(Provider.You, prompt=gpt_request["request"],chat=gpt_request["chat"])     
