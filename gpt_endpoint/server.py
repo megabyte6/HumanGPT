4c6f09d4e4615e81f0a7a47619f5f20bd78a2b00
Brayden Chan <braydenmchan@gmail.com>
2023-06-07 07:37:17 -0700

    Reset server.py

--- gpt_endpoint/server.py
+++ gpt_endpoint/server.py
@@ -43,7 +43,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         print("Request:", body.decode("utf-8"))
         self.request_from_gpt(data)
 
-    def request_from_gpt(self, gpt_request):
+    async def request_from_gpt(self, gpt_request):
         """
         Asynchronously handles a POST request from a client. Reads the request
         body, extracts a JSON object, creates a GPT-3 completion request using
@@ -55,7 +55,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
         gpt_request = json.loads(gpt_request)
         response = json.loads("{}")
-        response["response"] = Completion.create(
+        response["response"] = await Completion.create(
             Provider.You,
             prompt=gpt_request["request"],
             chat=gpt_request["chat"],
