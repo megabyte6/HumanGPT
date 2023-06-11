d2c58432bfa9761b6685d3968b042f76115348c1
Brayden Chan <braydenmchan@gmail.com>
2023-06-10 19:28:25 -0700

    Use a map to store votes instead of an array.

--- src/Voting.ts
+++ src/Voting.ts
@@ -1,55 +1,66 @@
 import Game from "./Game"
 import Player from "./Player"
 
-export class VotingGroup{
+export class VotingGroup {
 
-    votes: PlayerVotes[];
+    votes = new Map<Player, number>()
 
-    constructor(players: Player[]){
-        this.votes = players.map(player => new PlayerVotes(player));
-    }
-    
-    startVoting(game: Game){
-        game.handler?.start_voting(this.votes.length);
-
+    constructor(players: Player[]) {
+        players.forEach(player => this.votes.set(player, 0))
     }
 
-    addVote(idx: number){
-        this.votes[idx].addVote();
+    startVoting(game: Game) {
+        game.handler?.start_voting(this.votes.size);
     }
 
-    bestPlayer(){
-        let sorted: PlayerVotes[] = this.votes.sort((a,b)=>b.votes - a.votes);
-        return sorted[0].player
-
-
-
+    addVote(player: Player) {
+        let oldVotes = this.votes.get(player)
+        if (typeof oldVotes === "undefined") {
+            oldVotes = 0
+        }
+        this.votes.set(player, oldVotes + 1)
     }
 
-    assignPoints(){
-        let sorted: PlayerVotes[] = this.votes.sort((a,b)=>b.votes - a.votes);
-        sorted[0].player.score += 100;
-        sorted[1].player.score += 70;
-        sorted[2].player.score += 30;
-        sorted[3].player.score += 10;
-
+    bestPlayer() {
+        const rankedPlayers = this.rankPlayers()
+        const highestPoints = Array.from(rankedPlayers.entries()).reduce((a, b) => a[0] > b[0] ? a : b)[0]
+        return rankedPlayers.get(highestPoints)
     }
 
+    assignPoints() {
+        const pointsForRank = [100, 70, 30, 10]
 
-}
+        const rankedPlayers = this.rankPlayers()
+        const sortedRanks = Array.from(rankedPlayers.entries()).sort((a, b) => b[0] - a[0])
 
-export class PlayerVotes{
+        for (let i = 0; i < sortedRanks.length; i++) {
+            const rank = sortedRanks[i]
+            if (typeof rank === "undefined") {
+                break
+            }
 
-    player: Player;
-    votes: number;
-
-    constructor(player: Player){
-        this.player = player;
-        this.votes = 0;
+            rank[1].forEach((player: Player) => player.score += pointsForRank[i])
+        }
     }
 
-    addVote(){
-        this.votes++;
+    /**
+     * Ranks the players based on their votes and returns a Map object containing
+     * the rank and an array of players who received that rank.
+     *
+     * @return {Map<number, Player[]>} A Map object containing the rank and an array 
+     * of players who received that rank.
+     */
+    rankPlayers() {
+        const ranks = new Map<number, Player[]>()
+        this.votes.forEach((votes, player) => {
+            const rank = ranks.get(votes)
+            if (typeof rank === "undefined") {
+                ranks.set(votes, [player])
+                return
+            }
+            rank.push(player)
+        })
+        return ranks
     }
 
 }
